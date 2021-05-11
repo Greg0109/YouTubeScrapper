06dfe0d5de617c6e2dcef2841b5c66cc3384eace
Greg0109 <greg.rabago@gmail.com>
2021-05-11 02:31:45 +0200

    Updated for channels that changed the name

--- youtubescrapper.rb
+++ youtubescrapper.rb
@@ -3,6 +3,8 @@ require 'httparty'
 require 'nokogiri'
 require 'json'
 
+Youtubescrapper = ''
+
 class YouTubeScrapper
   def get_url_content(url)
       unparsed_page = HTTParty.get(url, {
@@ -17,11 +19,19 @@ class YouTubeScrapper
   end
 
   def get_videos_from_youtuber(youtuber)
-    url = "https://youtube.com/user/#{youtuber}/videos"
-    video_array = []
-    xml_content = get_url_content(url).to_s.split('ytInitialData')
-    filter_initial_data = xml_content[1].to_s.split(';</script><link rel="canonical"')
-    data_filtered = filter_initial_data[0].to_s.sub! ' = {','{'
+    begin
+      url = "https://youtube.com/user/#{youtuber}/videos"
+      video_array = []
+      xml_content = get_url_content(url).to_s.split('ytInitialData')
+      filter_initial_data = xml_content[1].to_s.split(';</script><link rel="canonical"')
+      data_filtered = filter_initial_data[0].to_s.sub! ' = {','{'
+    rescue
+      url = "https://youtube.com/c/#{youtuber}/videos"
+      video_array = []
+      xml_content = get_url_content(url).to_s.split('ytInitialData')
+      filter_initial_data = xml_content[1].to_s.split(';</script><link rel="canonical"')
+      data_filtered = filter_initial_data[0].to_s.sub! ' = {','{'
+    end
     data = JSON.parse(data_filtered)
     data.each do |key, value|
       if key == 'contents'
@@ -50,6 +60,7 @@ class YouTubeScrapper
                                         title = specs['title']['runs'][0]['text']
                                         video_hash.store('title', title)
                                         videoId = specs['videoId']
+                                        video_hash.store('videoid', videoId)
                                         videourl = "https://www.youtube.com/watch?v=#{videoId}"
                                         video_hash.store('videourl', videourl)
                                         published = specs['publishedTimeText']['simpleText']
@@ -108,6 +119,7 @@ class YouTubeScrapper
                                     title = specs['title']['runs'][0]['text']
                                     video_hash.store('title', title)
                                     videoId = specs['videoId']
+                                    video_hash.store('videoid', videoId)
                                     videourl = "https://www.youtube.com/watch?v=#{videoId}"
                                     video_hash.store('videourl', videourl)
                                     published = specs['publishedTimeText']['simpleText']
